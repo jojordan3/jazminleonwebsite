4596e207a5a3146a2d74dfe6c745dc9117f765ba
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-06-13 12:58:35 -0700

    wagtail stuff

--- jlcoaching/wsgi.py
+++ jlcoaching/wsgi.py
@@ -7,9 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-from django.core.management import execute_from_command_line
-execute_from_command_line('wagtail updatemodulepaths')
-
 from django.core.wsgi import get_wsgi_application
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jlcoaching.settings.dev")
