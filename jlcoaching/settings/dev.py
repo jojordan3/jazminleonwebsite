48fb98a202d692a67d13a829a7ae83f80c2e7a72
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-06-13 17:17:36 -0700

    adjust requirements and settings, update .gitignore

--- jlcoaching/settings/dev.py
+++ jlcoaching/settings/dev.py
@@ -8,7 +8,10 @@ SECRET_KEY = 'jup7!ka0b7n9fi@l22t^h^x_&qw#c!fxu(&vb(*i=mo+kqxmv#'
 
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.google.com'
+EMAIL_PORT = 587
 
 WAGTAIL_CACHE = False
 
--- jlcoaching/settings/prod.py
+++ jlcoaching/settings/prod.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+WAGTAIL_CACHE = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
