3742e47dfc3e7addac306b64a94dbd9e69165061
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-06-13 17:18:58 -0700

    update wsgi

--- jlcoaching/wsgi.py
+++ jlcoaching/wsgi.py
@@ -6,9 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
+#import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jlcoaching.settings.prod")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jlcoaching.settings.prod")
 
 application = get_wsgi_application()
